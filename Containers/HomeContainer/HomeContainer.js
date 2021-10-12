be24e1789efba0c40c2acdd7a9e41dd281824574
Omar <omaar_5@hotmail.com>
2021-10-12 22:29:26 +0200

    style: HomeContainer

--- Containers/HomeContainer/HomeContainer.js
+++ Containers/HomeContainer/HomeContainer.js
@@ -1,5 +1,13 @@
 import { Home } from "../../Components";
 import { HeadTag } from "../../Global";
+import {
+    HomeFooter,
+    LinksContainer,
+    UpperLinks,
+    LowerLinks,
+    Copy,
+    Link,
+} from "./styledHomeContainer";
 
 export const HomeContainer = () => {
     return (
@@ -7,6 +15,37 @@ export const HomeContainer = () => {
             <HeadTag title="instagram" />
 
             <Home />
+
+            <HomeFooter>
+                <LinksContainer>
+                    <UpperLinks>
+                        <Link>About</Link>
+                        <Link>Blog</Link>
+                        <Link>Jobs</Link>
+                        <Link>Help</Link>
+                        <Link>API</Link>
+                        <Link>Privacy</Link>
+                        <Link>Terms</Link>
+                        <Link>Top Accounts</Link>
+                        <Link>Hashtags</Link>
+                        <Link>Locations</Link>
+                        <Link>Instagram Lite</Link>
+                    </UpperLinks>
+                    <LowerLinks>
+                        <Link>Beauty</Link>
+                        <Link>Dance</Link>
+                        <Link>Fitness</Link>
+                        <Link>Food & Drink</Link>
+                        <Link>Home & Garden</Link>
+                        <Link>Music</Link>
+                        <Link>Visual Arts</Link>
+                    </LowerLinks>
+                </LinksContainer>
+
+                <Copy>
+                    <h6>© 2021 Instagram clone by <span>Omar Hassan</span></h6>
+                </Copy>
+            </HomeFooter>
         </div>
     );
 };
---
+++ Containers/HomeContainer/styledHomeContainer.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+export const HomeFooter = styled.footer`
+  display: flex;
+  flex-direction: column;
+  max-width: 700px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 1rem;
+`;
+
+export const LinksContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 1rem;
+`;
+
+export const UpperLinks = styled.div`
+  display: grid;
+  grid-template-columns: repeat(11, 1fr);
+  
+  @media screen and (max-width: 640px) {
+    grid-template-columns: repeat(6, 1fr);
+  }
+`;
+
+export const LowerLinks = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  @media screen and (max-width: 640px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+`;
+
+export const Copy = styled.div`
+  align-self: center;
+
+  span {
+    color: #f00;
+    cursor: pointer;
+  }
+`;
+
+export const Link = styled.div`
+  font-size: 0.6rem;
+  color: rgba(0, 0, 0, 0.5);
+  cursor: pointer;
+`;
